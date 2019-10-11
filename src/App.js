4bcd42b1d850b4750ffb292036d25f1ccee7935a
STM <startinmerc@gmail.com>
2019-10-11 12:00:46 +0200

    refactor morph functions

--- src/App.js
+++ src/App.js
@@ -13,28 +13,23 @@ class App extends React.Component {
 		this.state={
 			isCircle: false
 		}
-		this.toCircle = this.toCircle.bind(this);
-		this.toSquare = this.toSquare.bind(this);
+		this.svgMorph = this.svgMorph.bind(this);
 	}
-	
-	toCircle(){
-		if(!this.state.isCircle){
-			TweenMax.to("#rect1", 1, {attr:{rx:"50%", height:"80%", width:"55%", x:"22.5%", y:"10%"}, skewX: "0deg"});
-			this.setState({isCircle: true})
-		}
-	};
 
-	toSquare(){
+	svgMorph(){
 		if(this.state.isCircle){
 			TweenMax.to("#rect1", 1, {attr:{rx:"0%", height:"74%", width:"65%", x:"17.5%", y:"13%"}, skewX: "0deg"});
 			this.setState({isCircle: false})
+		} else {
+			TweenMax.to("#rect1", 1, {attr:{rx:"50%", height:"80%", width:"55%", x:"22.5%", y:"10%"}, skewX: "0deg"});
+			this.setState({isCircle: true})
 		}
 	};
 
 	render() {
 		return (
 			<div className="App">
-				<Nav toCircle={this.toCircle} toSquare={this.toSquare}/>
+				<Nav svgMorph={this.svgMorph}/>
 				<svg  xmlns="http://www.w3.org/2000/svg">
 					<rect id="rect1" rx="0" x="17.5%" y="13%" width="65%" height="74%" />  
 				</svg>
--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -10,16 +10,16 @@ class Nav extends React.Component {
 				<button id="stop">STOP</button>
 			</div>
 			<nav>
-				<a href="#hero" onClick={this.props.toSquare}>
+				<a href="#hero" onClick={this.props.svgMorph}>
 				<h6>
 					BEAT<br/>
 					FIRE
 				</h6>
 				</a>
 				<ul>
-					<li><a href="#book" onClick={this.props.toCircle}>BOOK</a></li>
-					<li><a href="#info" onClick={this.props.toSquare}>INFO</a></li>
-					<li><a href="#gigs" onClick={this.props.toCircle}>GIGS</a></li>
+					<li><a href="#book" onClick={this.props.svgMorph}>BOOK</a></li>
+					<li><a href="#info" onClick={this.props.svgMorph}>INFO</a></li>
+					<li><a href="#gigs" onClick={this.props.svgMorph}>GIGS</a></li>
 				</ul>
 			</nav>
 		</div>
