8d13c587cd9edcec12183d911fa3b80bdb0ea799
STM <startinmerc@gmail.com>
2019-10-11 11:58:49 +0200

    remove intersectionObserver, pass function to nav links instead

--- src/App.js
+++ src/App.js
@@ -13,52 +13,28 @@ class App extends React.Component {
 		this.state={
 			isCircle: false
 		}
-		this.createObserver = this.createObserver.bind(this);
+		this.toCircle = this.toCircle.bind(this);
+		this.toSquare = this.toSquare.bind(this);
 	}
 	
 	toCircle(){
-		TweenMax.to("#rect1", 1, {attr:{rx:"50%", height:"80%", width:"55%", x:"22.5%", y:"10%"}, skewX: "0deg"});
+		if(!this.state.isCircle){
+			TweenMax.to("#rect1", 1, {attr:{rx:"50%", height:"80%", width:"55%", x:"22.5%", y:"10%"}, skewX: "0deg"});
+			this.setState({isCircle: true})
+		}
 	};
 
 	toSquare(){
-		TweenMax.to("#rect1", 1, {attr:{rx:"0%", height:"74%", width:"65%", x:"17.5%", y:"13%"}, skewX: "0deg"});
+		if(this.state.isCircle){
+			TweenMax.to("#rect1", 1, {attr:{rx:"0%", height:"74%", width:"65%", x:"17.5%", y:"13%"}, skewX: "0deg"});
+			this.setState({isCircle: false})
+		}
 	};
-
-	createObserver() {
-		const observer = new IntersectionObserver((entries) => {
-		entries.forEach(entry => {
-				if(entry.intersectionRatio > 0 && this.state.isCircle) {
-					this.toSquare();
-					this.setState({isCircle: false});
-				} else if (entry.intersectionRatio > 0 && !this.state.isCircle){
-					this.toCircle();
-					this.setState({isCircle: true});
-				}
-			});
-		});
-		document.querySelectorAll('section').forEach((section)=>{
-			observer.observe(section, {threshold: 0.5});
-		})
-		// observer.observe(document.querySelector('#about'), {threshold: 0.1});
-		// observer.observe(document.querySelector('#contact'), {threshold: 0.1});
-		// observer.observe(document.querySelector('#gigs'), {threshold: 0.1});
-	}
-
-	componentDidMount(){
-		if (
-			"IntersectionObserver" in window &&
-			"IntersectionObserverEntry" in window &&
-			"intersectionRatio" in window.IntersectionObserverEntry.prototype
-		) {
-				this.createObserver();
-			};
-	}
-
 
 	render() {
 		return (
 			<div className="App">
-				<Nav />
+				<Nav toCircle={this.toCircle} toSquare={this.toSquare}/>
 				<svg  xmlns="http://www.w3.org/2000/svg">
 					<rect id="rect1" rx="0" x="17.5%" y="13%" width="65%" height="74%" />  
 				</svg>
@@ -66,9 +42,6 @@ class App extends React.Component {
 				<Book />
 				<Info />
 				<Gigs />
-				{/*
-				<script type="text/javascript" src="../Dropbox/Web Dev/Libraries/TweenMax.js"></script>
-			*/}
 			</div>
 		);
 	}
--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -10,14 +10,16 @@ class Nav extends React.Component {
 				<button id="stop">STOP</button>
 			</div>
 			<nav>
+				<a href="#hero" onClick={this.props.toSquare}>
 				<h6>
 					BEAT<br/>
 					FIRE
 				</h6>
+				</a>
 				<ul>
-					<li><a href="#book">BOOK</a></li>
-					<li><a href="#info">INFO</a></li>
-					<li><a href="#gigs">GIGS</a></li>
+					<li><a href="#book" onClick={this.props.toCircle}>BOOK</a></li>
+					<li><a href="#info" onClick={this.props.toSquare}>INFO</a></li>
+					<li><a href="#gigs" onClick={this.props.toCircle}>GIGS</a></li>
 				</ul>
 			</nav>
 		</div>
